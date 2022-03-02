32aebcb07786d96a2cfacaf18d5a6733968d6f6e
SakaguchiTakumi03 <sakaguchi.takumi03@gmail.com>
2022-03-03 06:31:06 +0900

    フレーム取得を失敗する場合を考慮

--- test.py
+++ test.py
@@ -6,9 +6,9 @@ camera = cv2.VideoCapture(0)
 # camera = cv2.VideoCapture(1)
 
 while True:
-    # time.sleep(1)
+    time.sleep(1)
     ret, frame = camera.read()
-    # time.sleep(1)
+    time.sleep(1)
     cv2.imshow('camera',frame)
 
     if cv2.waitLey(1) & 0xFF == ord('q'):
